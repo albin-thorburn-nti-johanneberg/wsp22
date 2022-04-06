40ca57a5904e8e5db246325daf26e4bb1dff28d8
albin-thorburn-nti-johanneberg <74763140+albin-thorburn-nti-johanneberg@users.noreply.github.com>
2022-04-06 16:18:48 +0200

    Fixat sessions för inloggning, genererat företag

--- app.rb
+++ app.rb
@@ -19,13 +19,32 @@ get('/company/new') do
     slim(:"company/new")
 end
 
+get('/company/view') do
+    db = SQLite3::Database.new("db/database.db")
+    id = session[:user_id]
+    name = db.execute("SELECT name FROM user WHERE id =?",id)
+    cash = db.execute("SELECT cash FROM user WHERE id =?",id)
+
+    companies = db.execute("SELECT")
+
+    slim(:"company/view", locals:{name:name, cash:cash, companies:})
+
+end
+
 get('/users/new') do
     slim(:"users/new")
 end
 
-
 get('/login') do
     slim(:"/login")
+end
+
+get('/delete') do
+    db = SQLite3::Database.new("db/database.db")
+    name = ""
+    db.execute("DELETE FROM user WHERE name =?",name)
+    db.execute("DELETE FROM company WHERE name =?",name)
+
 end
 
 post('/company/new') do
@@ -35,8 +54,17 @@ post('/company/new') do
     db = SQLite3::Database.new("db/database.db")
     result = db.execute("SELECT id FROM company WHERE name =?",name)
 
+    admin_id = 19
+    status = "ask"
+
     if result.empty?
         db.execute("INSERT INTO company (name, numberOfShares, startingPrice) VALUES (?,?,?)",name, number_of_shares, starting_price)
+
+        company_id = db.execute("SELECT id FROM company WHERE name =?",name)
+
+        number_of_shares.times do
+            db.execute("INSERT INTO stock (ownerId, companyId, price, status) VALUES (?,?,?,?)",admin_id, company_id, starting_price, status)
+        end
     end
     redirect('/')
 end
@@ -60,8 +88,11 @@ post('/users/login') do
     password = params[:password]
     db = SQLite3::Database.new("db/database.db")
     password_compare = db.execute("SELECT password FROM user WHERE name =?",name)
-    if BCrypt::Password.new(password_compare.first.first) == password 
-        p "lösenorden stämmer"
+    if BCrypt::Password.new(password_compare.first.first) == password
+        name = params[:name]
+        id = db.execute("SELECT id FROM user WHERE name =?",name)
+        session[:user_id] = id
+        redirect('/company/view') 
     else
         p "löseonrden stämmer inte"
     end
